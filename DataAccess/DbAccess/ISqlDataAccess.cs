b220891690b3b94a81caa483daa90df728a2a091
Madowwl <emma.arlanda@viacesi.fr>
2022-06-09 18:47:50 +0200

    Add DbAccess logic class + interface
    (prepare Dependency Injection)

---
+++ DataAccess/DbAccess/ISqlDataAccess.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace DataAccess.DbAccess
+{
+    public interface ISqlDataAccess
+    {
+        Task<IEnumerable<T>> LoadData<T, U>(string storedProcedure, U parameters, string connectionID = "Default");
+        Task SaveData<T>(string storedProcedure, T parameters, string connectionID = "Default");
+    }
+}
---
+++ DataAccess/DbAccess/SqlDataAccess.cs
@@ -0,0 +1,43 @@
+﻿
+
+using Dapper;
+using Microsoft.Extensions.Configuration;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Threading.Tasks;
+
+namespace DataAccess.DbAccess
+{
+    public class SqlDataAccess : ISqlDataAccess
+    {
+        private readonly IConfiguration _configuration;
+        public SqlDataAccess(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+        public async Task<IEnumerable<T>> LoadData<T, U>(string storedProcedure,
+                                                         U parameters,
+                                                         string connectionID = "Default")
+        {
+            using IDbConnection connection = new SqlConnection(_configuration.GetConnectionString(connectionID));
+
+            return await connection.QueryAsync<T>(storedProcedure,
+                                                  parameters,
+                                                  commandType: CommandType.StoredProcedure);
+        }
+
+        public async Task SaveData<T>(string storedProcedure,
+                                      T parameters,
+                                      string connectionID = "Default")
+        {
+            using IDbConnection connection = new SqlConnection(_configuration.GetConnectionString(connectionID));
+
+            await connection.ExecuteAsync(storedProcedure,
+                                          parameters,
+                                          commandType: CommandType.StoredProcedure);
+        }
+
+    }
+}
